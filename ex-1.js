8816c5a64b6825d47c74c58357458fe112ba2557
bestzaza789 <146432574+bestzaza789@users.noreply.github.com>
2023-12-14 10:31:03 +0700

    feat:finish all ex

--- ex-1.js
+++ ex-1.js
@@ -14,3 +14,10 @@ let getJohnProfile = () => {
   });
 };
 // Start coding here
+getJohnProfile()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
--- ex-4.js
+++ ex-4.js
@@ -13,3 +13,9 @@ let getJohnProfile = () => {
   });
 };
 // Start coding here
+async function asynchronousFunction() {
+  const johnProfile = await getJohnProfile();
+  console.log(johnProfile);
+}
+
+asynchronousFunction();
